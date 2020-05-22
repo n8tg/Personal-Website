9c08d9134321e08bc5d06ee7c505b2d3ffdb3d95
Nate Gramer <github@nategramer.com>
2020-05-21 22:23:43 -0500

    Update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -69,7 +69,8 @@ const IndexPage = () => (
               <p>
               At Prescient, I'm part of a "Do it all" team that's embedded onsite with our client.
 
-              Anything from server infrastructure, core networking, Active Directory, VDI, and application deployment, I'm behind it.
+              Anything from server infrastructure, core networking, Active Directory, VDI, and application deployment,
+              I'm behind it.
               </p>
             </div>
             <div className="resume-date text-md-right">
@@ -83,7 +84,8 @@ const IndexPage = () => (
               <h3 className="mb-0">Tier 2 Support</h3>
               <div className="subheading mb-3">Blue Star Tec</div>
               <p>
-              At Blue Star, I was a part of a 15 person MSP team. Serving on the second line of support, I learned how vital solid documentation, good note taking, and quality monitoring and alerting is to an organization.
+              At Blue Star, I was a part of a 15 person MSP team. Serving on the second line of support, I learned how
+              vital solid documentation, good note taking, and quality monitoring and alerting is to an organization.
               Being an MSP, I also gained invaluable experience with numerous business structures and environments.
               </p>
             </div>
@@ -141,14 +143,11 @@ const IndexPage = () => (
           <h2 className="mb-5">Skills</h2>
 
           <div className="subheading mb-3">
-            Programming Languages &amp; Tools
+            Familiar Operating Systems &amp; Tools
           </div>
           <ul className="list-inline dev-icons">
             <li className="list-inline-item">
               <i className="fab fa-aws"></i>
-            </li>
-            <li className="list-inline-item">
-              <i class="fas fa-network-wired"></i>
             </li>
             <li className="list-inline-item">
               <i class="fab fa-centos"></i>
@@ -163,49 +162,6 @@ const IndexPage = () => (
               <i class="fab fa-windows"></i>
             </li>
           </ul>
-
-          <div className="subheading mb-3">Workflow</div>
-          <ul className="fa-ul mb-0">
-            <li>
-              <i className="fa-li fa fa-check"></i>
-              Mobile-First, Responsive Design
-            </li>
-            <li>
-              <i className="fa-li fa fa-check"></i>
-              Cross Browser Testing &amp; Debugging
-            </li>
-            <li>
-              <i className="fa-li fa fa-check"></i>
-              Cross Functional Teams
-            </li>
-            <li>
-              <i className="fa-li fa fa-check"></i>
-              Agile Development &amp; Scrum
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <hr className="m-0" />
-
-      <section
-        className="resume-section p-3 p-lg-5 d-flex align-items-center"
-        id="interests"
-      >
-        <div className="w-100">
-          <h2 className="mb-5">Interests</h2>
-          <p>
-            Apart from being a web developer, I enjoy most of my time being
-            outdoors. In the winter, I am an avid skier and novice ice climber.
-            During the warmer months here in Colorado, I enjoy mountain biking,
-            free climbing, and kayaking.
-          </p>
-          <p className="mb-0">
-            When forced indoors, I follow a number of sci-fi and fantasy genre
-            movies and television shows, I am an aspiring chef, and I spend a
-            large amount of my free time exploring the latest technology
-            advancements in the front-end web development world.
-          </p>
         </div>
       </section>
 
@@ -220,34 +176,7 @@ const IndexPage = () => (
           <ul className="fa-ul mb-0">
             <li>
               <i className="fa-li fa fa-trophy text-warning"></i>
-              Google Analytics Certified Developer
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>
-              Mobile Web Specialist - Google Certification
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
-              Place - University of Colorado Boulder - Emerging Tech Competition
-              2009
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
-              Place - University of Colorado Boulder - Adobe Creative Jam 2008
-              (UI Design Category)
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>2<sup>nd</sup>
-              Place - University of Colorado Boulder - Emerging Tech Competition
-              2008
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
-              Place - James Buchanan High School - Hackathon 2006
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>3<sup>rd</sup>
-              Place - James Buchanan High School - Hackathon 2005
+              1st in state - US Cyber Challenge - 2011 - Reliving the Highschool glory days.
             </li>
           </ul>
         </div>
